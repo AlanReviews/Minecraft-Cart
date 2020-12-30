a94a22cba16afa4aa34f3e59f1dfeadf3697b4fe
Alan Zhou <54216387+AlanReviews@users.noreply.github.com>
2020-12-29 20:31:09 -0500

    Added more drawings and updated code
    - Added a line so the cart does not float
    - Added Steve by drawing rectangles

--- MinecraftCart.py
+++ MinecraftCart.py
@@ -6,13 +6,19 @@ def main():
 
     DISPLAY = pygame.display.set_mode((600,600),0,32)
 
-    WHITE=(255,255,255)
+    BG_COLOR=(169,169,169)
 
-    DISPLAY.fill(WHITE)
+    DISPLAY.fill(BG_COLOR)
 
-    pygame.draw.rect(DISPLAY,(211,211,211),(100,300,400,200))
-    pygame.draw.circle(DISPLAY,(0,0,0),(200,500),50)
-    pygame.draw.circle(DISPLAY,(0,0,0),(400,500),50)
+    pygame.draw.rect(DISPLAY, (211,211,211), (100,300,400,200))
+    pygame.draw.circle(DISPLAY, (0,0,0), (200,500), 50)
+    pygame.draw.circle(DISPLAY, (211,210,212), (200,500), 10)
+    pygame.draw.circle(DISPLAY, (0,0,0), (400,500), 50)
+    pygame.draw.circle(DISPLAY, (211,210,212), (400,500), 10)
+    pygame.draw.rect(DISPLAY, (14,174,174), (250,100,100,200))
+    pygame.draw.rect(DISPLAY, (169,125,100), (350,130,125,75))
+    pygame.draw.rect(DISPLAY, (101,67,33), (240,0,125,125))
+    pygame.draw.line(DISPLAY, (0,0,0), (0,550), (600,550), 2)
 
     while True:
         for event in pygame.event.get():
